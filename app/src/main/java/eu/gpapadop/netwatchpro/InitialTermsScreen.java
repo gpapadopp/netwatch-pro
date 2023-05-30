287acd4e247a187928a6b9e57766f6e605abc594
George Papadopoulos <70651728+gpapadopp@users.noreply.github.com>
2023-05-30 22:54:52 +0300

    Added initial terms screen page.

---
+++ app/src/main/java/eu/gpapadop/netwatchpro/InitialTermsScreen.java
@@ -0,0 +1,14 @@
+package eu.gpapadop.netwatchpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitialTermsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_terms_screen);
+    }
+}
--- app/src/main/java/eu/gpapadop/netwatchpro/MainActivity.java
+++ app/src/main/java/eu/gpapadop/netwatchpro/MainActivity.java
@@ -3,6 +3,7 @@ package eu.gpapadop.netwatchpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!this.hasAcceptTerms()){
+            //User Has NOT Accepted Terms
+            Intent intent = new Intent(getApplicationContext(), InitialTermsScreen.class);
+            startActivity(intent);
+            finish();
+        }
+
+    }
+
+    private boolean hasAcceptTerms(){
+        SharedPreferences sharedPrefs = getSharedPreferences("NetWatchProSharedPrefs", MODE_PRIVATE);
+        return sharedPrefs.getBoolean("accepted_terms", false);
     }
 }
