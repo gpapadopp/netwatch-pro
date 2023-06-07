c4e7c1e3e5aafcbf5e25f2aff8b9b1b7a52736e8
George Papadopoulos <70651728+gpapadopp@users.noreply.github.com>
2023-06-08 00:21:10 +0300

    Added always run app in background. Added package certificate getters.

--- app/src/main/java/eu/gpapadop/netwatchpro/MainActivity.java
+++ app/src/main/java/eu/gpapadop/netwatchpro/MainActivity.java
@@ -2,17 +2,23 @@ package eu.gpapadop.netwatchpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.net.Uri;
+import android.provider.Settings;
 
 import eu.gpapadop.netwatchpro.managers.installedApps.InstalledAppsHandler;
-import eu.gpapadop.netwatchpro.managers.installedApps.InstalledAppsManager;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Setup App to Always Run in Background
+        alwaysRunAppInBackground();
 
         Connectivity deviceConnectivity = new Connectivity(getApplicationContext());
         deviceConnectivity.initialize();
@@ -34,5 +40,17 @@ public class MainActivity extends AppCompatActivity {
     private boolean hasAcceptTerms(){
         SharedPreferences sharedPrefs = getSharedPreferences("NetWatchProSharedPrefs", MODE_PRIVATE);
         return sharedPrefs.getBoolean("accepted_terms", false);
+    }
+
+    @SuppressLint("BatteryLife")
+    private void alwaysRunAppInBackground(){
+        Intent intent = new Intent();
+        String packageName = getPackageName();
+        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+        if (!pm.isIgnoringBatteryOptimizations(packageName)){
+            intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package: " + packageName));
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/eu/gpapadop/netwatchpro/managers/installedApps/InstalledAppsHandler.java
+++ app/src/main/java/eu/gpapadop/netwatchpro/managers/installedApps/InstalledAppsHandler.java
@@ -4,7 +4,13 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-
+import android.content.pm.Signature;
+import android.content.pm.SigningInfo;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,6 +19,10 @@ public class InstalledAppsHandler extends InstalledAppsManager {
     private List<String> allPackageNames;
     private List<String> allRealNames;
     private List<List<String>> allPermissions;
+    private List<List<String>> allCertificateSubjects;
+    private List<List<String>> allCertificateIssuers;
+    private List<List<String>> allCertificateSerialNumbers;
+    private List<List<String>> allCertificateVersions;
 
     public InstalledAppsHandler(Context newAppContext){
         super(newAppContext);
@@ -20,6 +30,10 @@ public class InstalledAppsHandler extends InstalledAppsManager {
         this.allPackageNames = new ArrayList<>();
         this.allRealNames = new ArrayList<>();
         this.allPermissions = new ArrayList<>(new ArrayList<>());
+        this.allCertificateSubjects = new ArrayList<>(new ArrayList<>());
+        this.allCertificateIssuers = new ArrayList<>(new ArrayList<>());
+        this.allCertificateSerialNumbers = new ArrayList<>(new ArrayList<>());
+        this.allCertificateVersions = new ArrayList<>(new ArrayList<>());
     }
 
     public void initializeInstalledApps(){
@@ -35,19 +49,54 @@ public class InstalledAppsHandler extends InstalledAppsManager {
             this.allPackageNames.add(appPackageName);
             this.allRealNames.add(applicationInfo.loadLabel(packageManager).toString());
             this.allPermissions.add(new ArrayList<>());
+            this.allCertificateSubjects.add(new ArrayList<>());
+            this.allCertificateIssuers.add(new ArrayList<>());
+            this.allCertificateSerialNumbers.add(new ArrayList<>());
+            this.allCertificateVersions.add(new ArrayList<>());
         }
     }
 
     private void listAllPermissions(){
         PackageManager packageManager = super.getPackageManager();
-        List<PackageInfo> allPackageInfo = packageManager.getInstalledPackages(PackageManager.GET_PERMISSIONS|PackageManager.GET_RECEIVERS|PackageManager.GET_SERVICES|PackageManager.GET_PROVIDERS);
+        List<PackageInfo> allPackageInfo = packageManager.getInstalledPackages(PackageManager.GET_PERMISSIONS|PackageManager.GET_RECEIVERS|PackageManager.GET_SERVICES|PackageManager.GET_PROVIDERS|PackageManager.GET_SIGNING_CERTIFICATES);
         for (PackageInfo packageInfo : allPackageInfo){
             String[] packagePermissions = packageInfo.requestedPermissions;
+            //Get & Save Signatures
+            this.getPackageSignatures(packageInfo);
             if (packagePermissions != null) {
                 String packageName = packageInfo.packageName;
                 int packageIndex = this.allPackageNames.indexOf(packageName);
                 this.allPermissions.set(packageIndex, Arrays.asList(packagePermissions));
             }
         }
+    }
+
+    private void getPackageSignatures(PackageInfo packageInfo){
+        SigningInfo packageSigningInfo = packageInfo.signingInfo;
+        Signature[] packageSignatures = packageSigningInfo.getApkContentsSigners();
+        String packageName = packageInfo.packageName;
+        List<String> certificateSubjects = new ArrayList<>();
+        List<String> certificateIssuers = new ArrayList<>();
+        List<String> certificateSerialNumber = new ArrayList<>();
+        List<String> certificateVersions = new ArrayList<>();
+        for (Signature packageSignature : packageSignatures) {
+            byte[] rawCertificate = packageSignature.toByteArray();
+            InputStream certificateStream = new ByteArrayInputStream(rawCertificate);
+            try {
+                CertificateFactory certificateFactory = CertificateFactory.getInstance("X509");
+                X509Certificate x509Certificate = (X509Certificate) certificateFactory.generateCertificate(certificateStream);
+                certificateSubjects.add(x509Certificate.getSubjectDN().toString());
+                certificateIssuers.add(x509Certificate.getIssuerDN().toString());
+                certificateSerialNumber.add(x509Certificate.getSerialNumber().toString());
+                certificateVersions.add(String.valueOf(x509Certificate.getVersion()));
+            } catch (CertificateException ignored){
+
+            }
+        }
+        int packageIndex = this.allPackageNames.indexOf(packageName);
+        this.allCertificateSubjects.set(packageIndex, certificateSubjects);
+        this.allCertificateIssuers.set(packageIndex, certificateIssuers);
+        this.allCertificateSerialNumbers.set(packageIndex, certificateSerialNumber);
+        this.allCertificateVersions.set(packageIndex, certificateVersions);
     }
 }
