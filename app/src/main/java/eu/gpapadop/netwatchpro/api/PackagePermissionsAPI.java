07bfd78b493b62ca63ecf96a0a4c4abcb8f1af1b
George Papadopoulos <70651728+gpapadopp@users.noreply.github.com>
2023-06-11 17:40:25 +0300

    Fixed package permissions api add request.

--- app/src/main/java/eu/gpapadop/netwatchpro/api/PackagePermissionsAPI.java
+++ app/src/main/java/eu/gpapadop/netwatchpro/api/PackagePermissionsAPI.java
@@ -2,15 +2,14 @@ package eu.gpapadop.netwatchpro.api;
 
 import java.io.IOException;
 import java.util.List;
-
-import okhttp3.MediaType;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class PackagePermissionsAPI {
-    private final String apiURL = "http://127.0.0.1/api/v1/package-permissions/";
+    private final String apiURL = "https://arctouros.ict.ihu.gr/api/v1/package-permissions/";
 
     public void addPackagePermission(
             String deviceToken,
@@ -22,26 +21,14 @@ public class PackagePermissionsAPI {
             List<String> certificateSerialNumbers,
             List<String> certificateVersions
     ){
-        final String jsonBody = this.generateJsonString(
-                deviceToken,
-                packageName,
-                appName,
-                permissions,
-                certificateSubjects,
-                certificateIssuers,
-                certificateSerialNumbers,
-                certificateVersions
-        );
         final String addApiURL = this.apiURL + "add";
         try {
             OkHttpClient client = new OkHttpClient().newBuilder()
                     .build();
-            MediaType mediaType = MediaType.parse("application/json");
-            RequestBody body = RequestBody.create(mediaType, jsonBody);
             Request request = new Request.Builder()
                     .url(addApiURL)
-                    .method("POST", body)
-                    .addHeader("Content-Type", "application/json")
+                    .method("POST", this.generateBody(deviceToken, packageName, appName, permissions, certificateSubjects, certificateIssuers, certificateSerialNumbers, certificateVersions))
+                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
                     .build();
             Response response = client.newCall(request).execute();
         } catch (IOException ignored){
@@ -49,71 +36,37 @@ public class PackagePermissionsAPI {
         }
     }
 
-    private String generateJsonString(
+    private RequestBody generateBody(
             String deviceToken,
             String packageName,
             String appName,
-            List<String> permissions,
-            List<String> certificateSubjects,
-            List<String> certificateIssuers,
-            List<String> certificateSerialNumbers,
-            List<String> certificateVersions
+            List<String> allPermissions,
+            List<String> allCertificateSubjects,
+            List<String> allCertificateIssuers,
+            List<String> allCertificateSerialNumbers,
+            List<String> allCertificateVersions
     ){
-        String jsonString = "{";
-        jsonString += "\"device_token\":" + "\"" + deviceToken + "\",";
-        jsonString += "\"package_name\":" + "\"" + packageName + "\",";
-        jsonString += "\"app_name\":" + "\"" + appName + "\",";
-        if (permissions.size() != 0){
-            jsonString += "\"permissions\":" + "[";
-            jsonString += "\"" + permissions.get(0) + "\"";
-            for (int i = 1; i<permissions.size(); i++){
-                jsonString += ",\"" + permissions.get(i) + "\"";
+        RequestBody formBody = new FormBody.Builder()
+                .add("device_token", deviceToken)
+                .add("package_name", packageName)
+                .add("app_name", appName)
+                .add("permissions", this.generateStringFromList(allPermissions))
+                .add("certificate_subjects", this.generateStringFromList(allCertificateSubjects))
+                .add("certificate_issuers", this.generateStringFromList(allCertificateIssuers))
+                .add("certificate_serial_numbers", this.generateStringFromList(allCertificateSerialNumbers))
+                .add("certificate_versions", this.generateStringFromList(allCertificateVersions))
+                .build();
+        return formBody;
+    }
+
+    private String generateStringFromList(List<String> listToConvert){
+        if (listToConvert.size() > 1) {
+            String returnString = listToConvert.get(0);
+            for (int i = 1; i < listToConvert.size(); i++) {
+                returnString += "," + listToConvert.get(i);
             }
-            jsonString += "],";
-        } else {
-            jsonString += "\"permissions\":" + "[],";
+            return returnString;
         }
-        if (certificateSubjects.size() != 0){
-            jsonString += "\"certificate_subjects\":" + "[";
-            jsonString += "\"" + certificateSubjects.get(0) + "\"";
-            for (int i = 1; i<certificateSubjects.size(); i++){
-                jsonString += ",\"" + certificateSubjects.get(i) + "\"";
-            }
-            jsonString += "],";
-        } else {
-            jsonString += "\"certificate_subjects\":" + "[],";
-        }
-        if (certificateIssuers.size() != 0){
-            jsonString += "\"certificate_issuers\":" + "[";
-            jsonString += "\"" + certificateIssuers.get(0) + "\"";
-            for (int i = 1; i<certificateIssuers.size(); i++){
-                jsonString += ",\"" + certificateIssuers.get(i) + "\"";
-            }
-            jsonString += "],";
-        } else {
-            jsonString += "\"certificate_issuers\":" + "[],";
-        }
-        if (certificateSerialNumbers.size() != 0){
-            jsonString += "\"certificate_serial_numbers\":" + "[";
-            jsonString += "\"" + certificateSerialNumbers.get(0) + "\"";
-            for (int i = 1; i<certificateSerialNumbers.size(); i++){
-                jsonString += ",\"" + certificateSerialNumbers.get(i) + "\"";
-            }
-            jsonString += "],";
-        } else {
-            jsonString += "\"certificate_serial_numbers\":" + "[],";
-        }
-        if (certificateVersions.size() != 0){
-            jsonString += "\"certificate_versions\":" + "[";
-            jsonString += "\"" + certificateVersions.get(0) + "\"";
-            for (int i = 1; i<certificateVersions.size(); i++){
-                jsonString += ",\"" + certificateVersions.get(i) + "\"";
-            }
-            jsonString += "]";
-        } else {
-            jsonString += "\"certificate_versions\":" + "[]";
-        }
-        jsonString += "}";
-        return jsonString;
+        return "";
     }
 }
