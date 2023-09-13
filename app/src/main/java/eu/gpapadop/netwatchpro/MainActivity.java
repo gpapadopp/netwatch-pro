19d8e54b5aa6271deadcb9085ee2101d65d18064
George Papadopoulos <70651728+gpapadopp@users.noreply.github.com>
2023-09-14 01:57:00 +0300

    Added custom toolbar for the app.

--- app/src/main/java/eu/gpapadop/netwatchpro/MainActivity.java
+++ app/src/main/java/eu/gpapadop/netwatchpro/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.os.PowerManager;
 import android.net.Uri;
 import android.provider.Settings;
+import android.widget.Toolbar;
 
 import java.util.Calendar;
 
@@ -21,86 +22,9 @@ import eu.gpapadop.netwatchpro.managers.internetPackages.PackageCaptureWifi;
 import eu.gpapadop.netwatchpro.notifications.NotificationsHandler;
 
 public class MainActivity extends AppCompatActivity {
-    private AlarmManager installedAppsAlarmManager;
-    private static final int INSTALLED_APPS_REQUEST_CODE = 1;
-    private NotificationsHandler notificationsHandler;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Setup App to Always Run in Background
-        alwaysRunAppInBackground();
-
-        //Get Phone Token ID
-        Connectivity deviceConnectivity = new Connectivity(getApplicationContext());
-        deviceConnectivity.initialize();
-        String token = deviceConnectivity.getDeviceID();
-
-        //Get Installed Phone Apps
-        InstalledAppsHandler installedAppsManager = new InstalledAppsHandler(getApplicationContext(), token);
-        installedAppsManager.initializeInstalledApps();
-        //Setup Repeater for Installed Apps Manager - Every 24 Hours
-        registerInstalledAppsAlarmReceiver();
-
-        //Setup Package Capture Process
-        Intent vpnIntent = VpnService.prepare(this);
-        if (vpnIntent != null) {
-            startActivityForResult(vpnIntent, 0);
-        } else {
-            onActivityResult(0, RESULT_OK, null);
-        }
-
-        //Setup Notification
-        this.notificationsHandler = new NotificationsHandler(getApplicationContext());
-        this.notificationsHandler.showStickyNotification(getApplicationContext().getString(R.string.app_is_running), getApplicationContext().getString(R.string.the_app_is_analyzing_your_traffic_and_your_installed_apps));
-
-
-//        if (!this.hasAcceptTerms()){
-//            //User Has NOT Accepted Terms
-//            Intent intent = new Intent(getApplicationContext(), InitialTermsScreen.class);
-//            startActivity(intent);
-//            finish();
-//        }
-
-    }
-
-    private boolean hasAcceptTerms(){
-        SharedPreferences sharedPrefs = getSharedPreferences("NetWatchProSharedPrefs", MODE_PRIVATE);
-        return sharedPrefs.getBoolean("accepted_terms", false);
-    }
-
-    @SuppressLint("BatteryLife")
-    private void alwaysRunAppInBackground(){
-        Intent intent = new Intent();
-        String packageName = getPackageName();
-        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
-        if (!pm.isIgnoringBatteryOptimizations(packageName)){
-            intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-            intent.setData(Uri.parse("package: " + packageName));
-            startActivity(intent);
-        }
-    }
-
-    private void registerInstalledAppsAlarmReceiver(){
-        this.installedAppsAlarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        // Set the alarm to start at approximately 24 hours from now
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_YEAR, 1);
-        // Create an Intent for the AlarmReceiver class
-        Intent intent = new Intent(this, InstalledAppsAlarmReceiver.class);
-        // Create a PendingIntent to be triggered when the alarm goes off
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, INSTALLED_APPS_REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
-        // Set the alarm to repeat every 24 hours
-        this.installedAppsAlarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            Intent vpnIntent = new Intent(this, PackageCaptureWifi.class);
-            startService(vpnIntent);
-        }
     }
 }
