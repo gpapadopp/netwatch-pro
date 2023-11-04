0bda5973c5fa8ff7a9ca2b1eb78f629904f7a39e
George Papadopoulos <70651728+gpapadopp@users.noreply.github.com>
2023-11-04 15:20:06 +0200

    Added loading circular progress on scan your apps activity.

--- app/src/main/java/eu/gpapadop/netwatchpro/ScanYourAppsActivity.java
+++ app/src/main/java/eu/gpapadop/netwatchpro/ScanYourAppsActivity.java
@@ -3,12 +3,41 @@ package eu.gpapadop.netwatchpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class ScanYourAppsActivity extends AppCompatActivity {
+    int i = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_your_apps);
+
+        this.handleProgressBar();
+    }
+
+    private void handleProgressBar(){
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        TextView progressBarTextView = (TextView) findViewById(R.id.progress_text);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // set the limitations for the numeric
+                // text under the progress bar
+                if (i <= 100) {
+                    progressBarTextView.setText("" + i);
+                    progressBar.setProgress(i);
+                    i++;
+                    handler.postDelayed(this, 200);
+                } else {
+                    handler.removeCallbacks(this);
+                }
+            }
+        }, 200);
     }
 }
